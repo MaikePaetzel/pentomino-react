a92225ba3876d1c4e7a4c16d80be1aeab0f96e39
kfriedrichs <friedrichs@uni-potsdam.de>
2021-03-10 11:14:20 +0100

    moving, rotating, flipping, fixing correct pieces fully implemented; fixed highlighting

--- pentomino/src/App.js
+++ pentomino/src/App.js
@@ -6,7 +6,7 @@ import React, { useEffect, useReducer, useState, useRef } from 'react'
 import { PentoBoard } from "./pento-objects/PentoBoard";
 import { PentoConfig } from "./config";
 import { configPerShape, createNewPentoPieceInShape, generateElephantShape } from "./pento-objects/HelperDrawComplexShapes";
-import {grid_cell_to_coordinates} from "./pento-objects/HelperDrawingBoard";
+import { grid_cell_to_coordinates, coordinates_to_grid_cell } from "./pento-objects/HelperDrawingBoard";
 import Furhat from 'furhat-gui'
 
 
@@ -41,7 +41,8 @@ const App = () => {
       "startTime": undefined,
       "time": game_time,
     },
-    "selected": "None"
+    "selected": "None",
+    "selected_coords": []
   }
 
 
@@ -103,6 +104,14 @@ const App = () => {
           selected: "None"
         };
       }
+      case 'updateCoords': {
+        // Pixelkoordinaten in Gitterkoordinaten umrechnen
+        // falls kein Stein aktiv, leeres Array speichern
+        return {
+          ...state,
+          selected_coords: (action.x < 0 || action.y < 0) ? [] : coordinates_to_grid_cell(action.x, action.y, grid_config.block_size)
+        }
+      }
       case 'addToRightBoard':
         return {
           ...state,
@@ -114,18 +123,18 @@ const App = () => {
           left_board: [...state.left_board, action.piece]
         };
       case 'removeFromLeftBoard':
-        let filtered_list = state.left_board.filter(item => item.name !== action.piece.name)
+        let new_left_board = state.left_board.filter(item => item.name !== action.piece.name)
         return {
           ...state,
-          left_board: filtered_list
+          left_board: new_left_board
         };
-
       case 'pieceAtGoal':
+        let new_right_board = state.right_board.filter(item => item.name !== action.piece.name);
         return {
           ...state,
+          right_board: new_right_board,
           correctly_placed: [...state.correctly_placed, action.piece]
         };
-
       case 'gameWon':
         return {
           ...state,
@@ -145,7 +154,8 @@ const App = () => {
             "startTime": undefined,
             "time": game_time,
           },
-          "selected": "None"
+          "selected": "None",
+          "selected_coords": []
         }
       case 'refreshTime':
 
@@ -229,20 +239,21 @@ const App = () => {
    * @param pento_name Der Name des Pentomino-Teils als String
    */
   const selectPentoPiece = (pento_name) => {
-    //todo: stop move here?
     if (activeShape.length > 0 && activeShape[0].name == pento_name) {
       deselect();
     } else {
+      // Evtl. laufende Bewegung des alten aktiven Steins stoppen.
+      stopMove();
       setActiveShape(initialShapes.concat(placedShapes).filter(item => item.name == pento_name.toString()));
     }
   };
-
   /**
    * Diese Methode sorgt dafür, dass alle momentan ausgewählten Spielsteine nicht mehr ausgewählt sind
    * Wird entweder durch Button-Klick oder Event vom Roboter aufgerufen.
    */
   const deselect = () => {
-    setActiveShape([])
+    stopMove();
+    setActiveShape([]);
   };
 
   /** 
@@ -259,41 +270,34 @@ const App = () => {
     return (activeShape[0] && placedShapes.find(shape => shape.name == activeShape[0].name));
   }
 
-  // Parameter und Variblen zur Bewegung des aktiven Spielsteins auf dem rechten Spielbrett
+  // Parameter und Variablen zur Bewegung des aktiven Spielsteins auf dem rechten Spielbrett
   const [MOVESPEED, setMOVESPEED] = useState(5);
-  const [MOVEFREQ, setMOVEFREQ]  = useState(200);
-  const moveHandler    = useRef(null);
+  const [MOVEFREQ, setMOVEFREQ]   = useState(200);
+  const moveHandler               = useRef(null);
 
   /**
-  * Move the active shape on some PentoBoard by a delta x, y
-  * @param {PentoBoard reference} board
-  * @param {x distance} dx
-  * @param {y distance} dy
+  * Den aktiven Stein um (dx,dy) verschieben.
+  * @param {Entfernung horizontal} dx
+  * @param {Entfernung vertikal} dy
   */
   const moveActive = (dx, dy) => {
     let active = activeShape[0];
-    // oder so?
-    // setActiveShape(placedShapes.map(shape => {
-    //     if (shape.name == active.name) {
-    //       shape.moveTo(shape.x+dx, shape.y+dy);
-    //     }
-    //     return shape;
-    //   }));
+    // den Gamestate mit den neuen Koordinaten updaten
+    dispatch({type: 'updateCoords', x:active.x+dx, y:active.y+dy});
     active.moveTo(active.x+dx, active.y+dy);
-    //TODO: Hier muss das dispatch noch rein, damit der gameState auch richtig geupdated wird
-    //setActiveShape([active.copy(active.id)]);
   };
 
   /**
-   * Stop current interval and set an interval for moving the active shape
-   * on the document.board
-   * @param {one of [up, down, left, right]} dir
-   * @param {move frequency in milliseconds, default:500} interval
-   * @param {distance of each step in pixels, default: 5} step
+   * Eine aktuelle Bewegung des aktiven Spielsteins stoppen und eine neue Bewegung starten.
+   * @param {eine Richtung aus ['up', 'down', 'left', 'right']} dir
+   * @param {Häufigkeit der Bewegung, default:200} interval
+   * @param {mit jedem Schritt zurückgelegte Pixel, default: 5} step
    */
   const startMove = (dir, interval=MOVEFREQ, step=MOVESPEED) => {
-    stopMove();
     if (activeOnRightBoard()) {
+      // evtl. stattfindende Bewegung anhalten, aber für eine flüssige Richtungsänderung
+      // Stein nicht ein
+      stopMove(false);
       // setInterval wird genutzt, um die Funktion moveActive in regelmäßigen Abständen auszuführen
       switch (dir) {
         case 'up':
@@ -319,34 +323,26 @@ const App = () => {
   /**
   * Bewegung des aktiven Spielstein anhalten und den Stein auf einem Quadrat der Matrix 'einrasten' lassen
   */
-  const stopMove = () =>  {
+  const stopMove = (lock_and_fix_correct=true) =>  {
     clearInterval(moveHandler.current);
-    if (activeOnRightBoard()) {
+    if (lock_and_fix_correct && activeOnRightBoard()) {
       lockActiveOnGrid();
-      fixCorrectlyPlaced();
+      fixCorrectlyPlaced(); // falls Stein korrekt liegt: fixieren
     }
   }
   
-
   /**
   * Den aktiven Spielstein auf dem rechten Brett rotieren, um delta_angle Grad
   */
   const rotateActive = (delta_angle) => {
     if (activeShape.length > 0) {
-      let active = activeShape[0].name;
-      // Aktive Shape rotieren, alle anderen bleiben unverändert. 
-      // Nach dem Rotieren testen ob Shape in Zielkonfiguration ist und evtl. dort fixieren.
-      setPlacedShapes(placedShapes.map(shape => {
-        if (shape.name == active) {
-          shape.rotate(delta_angle);
-          fixCorrectlyPlaced(shape);
-        }
-        return shape;
-      }));
+      let active = activeShape[0];
+      // Aktive Shape rotieren; testen, ob Shape in Zielkonfiguration ist und evtl. dort fixieren
+      active.rotate(delta_angle);
+      fixCorrectlyPlaced(active);
     } else {
       console.log('No active shape');
     }
-    dispatch({type: 'rotate', angle: delta_angle})
   }
   
   /**
@@ -354,16 +350,10 @@ const App = () => {
    */
   const flipActive = (axis) => {
     if (activeShape.length > 0) {
-      let active = activeShape[0].name;
-      // Aktive Shape spiegeln, alle anderen bleiben unverändert. 
-      // Nach dem Spiegeln testen, ob Shape in Zielkonfiguration ist und evtl. dort fixieren.
-      setPlacedShapes(placedShapes.map(shape => {
-        if (shape.name == active) {
-          shape.flip(axis);
-          fixCorrectlyPlaced(shape);
-        }
-        return shape;
-      }));
+      let active = activeShape[0];
+      // Aktive Shape spiegeln; testen, ob Shape in Zielkonfiguration ist und evtl. dort fixieren.
+      active.flip(axis);
+      fixCorrectlyPlaced(active);
     } else {
       console.log('No active shape');
     }
@@ -389,13 +379,14 @@ const App = () => {
     if (shape.is_mirrored) { return false; }
     let goalCoords = configPerShape("elephant", grid_config.n_blocks);
     goalCoords = grid_cell_to_coordinates(goalCoords['x'] + goalCoords['coords'][shape.type]['x'],
-                                          goalCoords['y'] + goalCoords['coords'][shape.type]['y']);
-    if (shape.x != goalCoords[0] || shape.y != goalCoords[1]) { return false };
+                                          goalCoords['y'] + goalCoords['coords'][shape.type]['y'],
+                                          grid_config.block_size);
+    if (shape.x != goalCoords[0] || shape.y != goalCoords[1]) { return false; };
     // Die Zielrotation ist 0. Das 'rotation'-Attribut eines Steins kann jedoch nicht einfach
     // verwendet werden, da der Wert bei Verwendung einer Spiegelung ('flip') verfälscht
     // werden kann. Hier wird eine etwas 'hacky' Lösung verwendet: Es wird eine neue Shape
     // desselben Typs erstellt und die Blockanordnung abgeglichen.
-    let dummy_shape = createNewPentoPieceInShape("dummy", grid_config, shape.type, "black", -1);
+    let dummy_shape = createNewPentoPieceInShape("elephant", grid_config, shape.type, "black", -1);
     let shape_grid = shape.get_internal_grid();
     let dummy_grid = dummy_shape.get_internal_grid();
     for (let row = 0; row < shape.get_grid_height(); row++) {
@@ -426,7 +417,7 @@ const App = () => {
       new_x = Math.floor((new_x - grid_config.x) / grid_config.block_size) * grid_config.block_size;
       new_y = Math.floor((new_y - grid_config.y) / grid_config.block_size) * grid_config.block_size;
       active.moveTo(new_x, new_y);
-      setActiveShape(active.copy(active.id));
+      dispatch({type: 'updateCoords', x:new_x, y:new_y});
     } else {
       console.log('No active shape');
     }
@@ -464,7 +455,9 @@ const App = () => {
       // Falls der Spielstein auf dem linken Brett nicht gefunden wurde, gibt es nichts zu tun
       if (!to_replace) { return; }
 
+      // Kopie des aktiven Stein erstellen, mit anderer Position, aber gleicher Rotation und Spiegelung
       let new_shape = createNewPentoPieceInShape("upper_left_corner", grid_config, to_replace.type, to_replace.color, to_replace.id);
+
 
       const newPiece = pentoPieceToObj(new_shape.name, new_shape.type, new_shape.color, new_shape.x, new_shape.y);
       dispatch({type: 'addToRightBoard', piece: newPiece});
@@ -564,9 +557,11 @@ const App = () => {
   useEffect(() => {
     if (activeShape && activeShape.length > 0) {
       dispatch({type: 'selectPiece', piece: activeShape[0]});
+      dispatch({type: 'updateCoords', x:activeShape[0].x, y:activeShape[0].y});
     }
     else {
-      dispatch({type: 'deselectPiece'})
+      dispatch({type: 'deselectPiece'});
+      dispatch({type: 'updateCoords', x:-1, y:-1});
     }
   }, [activeShape]);
 
@@ -576,7 +571,6 @@ const App = () => {
   useEffect(() => {
     sendDataToFurhat()
   }, [gameState.game.time]);
-
 
   /**
    * Wenn die Web-UI initialisiert wird, verbinden wir uns mit dem Roboter.
